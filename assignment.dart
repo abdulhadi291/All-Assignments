6a1b37bfdf16e4dde6c81fc31e931dbfde7228d3
Abdul Hadi <125742871+abdulhadi291@users.noreply.github.com>
2025-04-18 22:59:21 +0500

    assignment no. 2

--- assignment.dart
+++ assignment.dart
@@ -1,12 +1,75 @@
 // Assignment 1
 
+// void main() {
+//   String name = "Abdul Hadi";
+//   int age = 22;
+//   num percentage = 88.7;
+//   num previous_result = 80;
+//   print(name);
+//   print(age);
+//   print(percentage);
+//   print(previous_result);
+// }
+
+//----------------------------------------------------------------------
+//-----------------------------------------------------------------
+
+// Assignment 2
+
+// void main() {
+//   num tempInCelcius = 36;
+//   num tempInFarenhiet = 98;
+
+//   num celciusToFarenhiet = (tempInCelcius * 9 / 5) + 32;
+//   num farenhietToCelcius = (tempInFarenhiet - 32) * 5 / 9;
+
+//   print("Temp in Farenhiet $tempInFarenhiet");
+//   print("Temp converted to Celcius $farenhietToCelcius");
+
+//   print("Temp in Celcius $tempInCelcius");
+//   print("Temp converted to Farenhiet $celciusToFarenhiet");
+// }
+
+//----------------------------------------------------------------------
+
+// void main() {
+//   num length = 5;
+//   num breadth = 7;
+//   num area = length * breadth;
+
+//   print("Area of triangle is $area");
+// }
+
+//----------------------------------------------------------------------
+
+// void main() {
+//   num numb = 7;
+//   numb = numb + 8;
+//   numb = numb / 3;
+//   numb = numb % 5;
+//   numb = numb * 5;
+//   print(numb);
+// }
+
+//----------------------------------------------------------------------
+
 void main() {
-  String name = "Abdul Hadi";
-  int age = 22;
-  num percentage = 88.7;
-  num previous_result = 80;
-  print(name);
-  print(age);
-  print(percentage);
-  print(previous_result);
+  String name = "Hadi";
+  num totalMarks = 300;
+  num subject1 = 78;
+  num subject2 = 45;
+  num subject3 = 62;
+  num percentage;
+  num obtainedMarks;
+
+  obtainedMarks = subject1 + subject2 + subject3;
+  percentage = (obtainedMarks / totalMarks) * 100;
+
+  print("Name is $name");
+  print("Obtained Marks are $obtainedMarks");
+  print("Total Marks are $totalMarks");
+  print("Percentage is $percentage");
 }
+
+//----------------------------------------------------------------------
+//-----------------------------------------------------------------
