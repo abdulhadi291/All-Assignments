7103c0cdf5b5ca2a631756b8e010ce3d05801324
Abdul Hadi <125742871+abdulhadi291@users.noreply.github.com>
2025-04-18 22:57:23 +0500

    assignment no 1

---
+++ assignment.dart
@@ -0,0 +1,12 @@
+// Assignment 1
+
+void main() {
+  String name = "Abdul Hadi";
+  int age = 22;
+  num percentage = 88.7;
+  num previous_result = 80;
+  print(name);
+  print(age);
+  print(percentage);
+  print(previous_result);
+}
